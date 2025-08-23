4d24a786f17a527f89394dfb1aaea270902e525b
vishal prajapati <vishalprajapati43968@gmail.com>
2025-08-23 13:55:26 +0530

    setup vite + tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-950">
+      <div className="p-8 rounded-2xl border border-slate-800 bg-slate-900 shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-emerald-400">
+          Tailwind + Vite Plugin ✅
+        </h1>
+        <p className="mt-4 text-slate-300">Modern setup working perfectly!</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
